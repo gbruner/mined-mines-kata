d6d874ea1f59dcb814fd7f077f2826fce4e6f1fa
George Bruner <gbruner@gmail.com>
2017-01-18 07:37:17 -0500

    add conditional print for divisibles

--- kata.rb
+++ kata.rb
@@ -1,3 +1,21 @@
 (1..100).each do |i|
-	puts i
+	if i % 3 == 0 and i % 5 == 0
+		
+		puts "mined minds"
+
+	elsif i % 3 == 0
+	
+	    puts "mined"	
+
+	elsif i % 5 == 0
+
+		puts "minds"
+
+	else
+puts i
 end
+end
+
+
+
+
